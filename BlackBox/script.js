d3d5c4c5aee01ba6936119a9be98599aac9faf58
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 15:48:56 +0200

    full dist compute

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -27,11 +27,19 @@ function update(width=0.8){
 
 function check_points(){
     test_pts_main.innerHTML = "";
-    const N = [1/Math.sqrt(A*A + 1), A/Math.sqrt(A*A + 1)] // normal to line, length 1
-    const Q = [0,B] // belongs to the line
+    // normal to line, length 1
+    const N = [1/Math.sqrt(A*A + 1), A/Math.sqrt(A*A + 1)];
+    // vector belonging to the line
+    const Q = [0,B];
     for (let i = 0; i < PTS.length; i++) {
-        const pt = PTS[i];
-        test_pts_main.innerHTML += `<ellipse style="fill:#00ff00" cx="${pt[0]+50}" cy="${pt[1]+50}" rx="2" ry="2"></ellipse>\n`
+        const P = PTS[i];
+        // V = P-Q
+        const V = [P[0]-Q[0], P[1]-Q[1]];
+        // N.V = |N|*|V|*cos(N_V)
+        // d = |V|*cos(N_V) = N.V/|N|
+        // as |N|=1, d=N.V
+        const d = N[0]*V[0] +  N[1]*V[1];
+        test_pts_main.innerHTML += `<ellipse style="fill:#00ff00" cx="${P[0]+50}" cy="${P[1]+50}" rx="2" ry="2"></ellipse>\n`
     }
 }
 
