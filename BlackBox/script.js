b9ed40cb635bdda3181d438205744328a7bca8ee
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 12:52:36 +0200

    test update line

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -1,2 +1,13 @@
 
 console.log(input_a.value)
+
+function update(){
+    var x0 = 0;
+    var y0 = 0;
+    var x1 = 100;
+    var y1 = 10;
+    var dx = x1-x0;
+    var dy = y1-y0;
+    line.outerHTML = `<path style="stroke:#000000;stroke-width:0.3;stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
+
+}
