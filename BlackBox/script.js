d6880675d970c0f5c6fb73f90d4ba4fea3263dc8
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 16:16:47 +0200

    valid / invalid pts display

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -17,12 +17,13 @@ function update(width=0.8){
     var x0 = 0;
     A = Number(Math.tan(input_a.value));
     B = A*Number(input_c.value) + Number(input_b.value)
-    var y0 = A*(50+Number(input_c.value)) + Number(input_b.value) +50;
+    var y0 = A*(-50+Number(input_c.value)) + Number(input_b.value) +50;
     var x1 = 100;
-    var y1 = A*(-50+Number(input_c.value)) + Number(input_b.value) +50;
+    var y1 = A*(50+Number(input_c.value)) + Number(input_b.value) +50;
     var dx = x1-x0;
     var dy = y1-y0;
     line.outerHTML = `<path style="stroke:#000000;stroke-width:${width};stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
+    check_points();
 }
 
 function check_points(){
@@ -41,7 +42,11 @@ function check_points(){
         // d = |V|*cos(N_V) = N.V/|N|
         // as |N|=1, d=N.V
         const d = Math.abs(N[0]*V[0] + N[1]*V[1]);
-        test_pts_main.innerHTML += `<ellipse style="fill:#00ff00" cx="${P[0]+50}" cy="${P[1]+50}" rx="2" ry="2"></ellipse>\n`
+        if(d<3){
+            test_pts_main.innerHTML += `<ellipse style="fill:#00ff00" cx="${P[0]+50}" cy="${P[1]+50}" rx="2" ry="2"></ellipse>\n`
+        } else{
+            test_pts_main.innerHTML += `<ellipse style="fill:#ff0000" cx="${P[0]+50}" cy="${P[1]+50}" rx="2" ry="2"></ellipse>\n`
+        }
     }
 }
 
