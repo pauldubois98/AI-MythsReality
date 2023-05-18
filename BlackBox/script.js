d0d3bd1b375b3d40fa68258b897abb2ac9eac19b
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 14:17:31 +0200

    temp update on mousemove full update else

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -1,5 +1,5 @@
 
-function update(){
+function update(width=0.8){
     // y = a (x-c) + b
     var x0 = 0;
     var y0 = Number(Math.tan(input_a.value))*(50-Number(input_c.value)) + Number(input_b.value) +50;
@@ -7,7 +7,7 @@ function update(){
     var y1 = Number(Math.tan(input_a.value))*(-50-Number(input_c.value)) + Number(input_b.value) +50;
     var dx = x1-x0;
     var dy = y1-y0;
-    line.outerHTML = `<path style="stroke:#000000;stroke-width:0.3;stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
+    line.outerHTML = `<path style="stroke:#000000;stroke-width:${width};stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
 }
 
 update();
