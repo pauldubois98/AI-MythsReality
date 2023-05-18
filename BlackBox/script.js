53c9f6e508eb7e05de8462056a517af7dc288b71
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 16:36:25 +0200

    better math

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -21,10 +21,10 @@ function draw_line(width=0.8){
     // y = a (x-c) + b
     var x0 = 0;
     A = Number(Math.tan(input_a.value));
-    B = A*Number(input_c.value) + Number(input_b.value)
-    var y0 = A*(-50+Number(input_c.value)) + Number(input_b.value) +50;
+    B = -A*Number(input_c.value) + Number(input_b.value)
+    var y0 = A*(-50-Number(input_c.value)) + Number(input_b.value) +50;
     var x1 = 100;
-    var y1 = A*(50+Number(input_c.value)) + Number(input_b.value) +50;
+    var y1 = A*(50-Number(input_c.value)) + Number(input_b.value) +50;
     var dx = x1-x0;
     var dy = y1-y0;
     line.outerHTML = `<path style="stroke:#000000;stroke-width:${width};stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
