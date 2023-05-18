b10b9f78f1f419554e8e8a45f43975cba463cae4
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 13:15:18 +0200

    update line according to inputs

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -4,9 +4,10 @@ console.log(input_a.value)
 function update(){
     // y = a (x-c) + b
     var x0 = 0;
-    var y0 = 0;
+    var y0 = Number(input_a.value)*(50-Number(input_c.value)) + Number(input_b.value) +50;
     var x1 = 100;
-    var y1 = input_a.value;
+    var y1 = Number(input_a.value)*(-50-Number(input_c.value)) + Number(input_b.value) +50;
+    console.log(input_a.value, input_b.value, input_c.value)
     var dx = x1-x0;
     var dy = y1-y0;
     line.outerHTML = `<path style="stroke:#000000;stroke-width:0.3;stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
