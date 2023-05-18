612eae5c05a848c696caa0cbbd39eea8f588f970
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 15:27:23 +0200

    global var A & B

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -1,16 +1,16 @@
-PTS = [
-    [-50,10],
-    [-25,10],
-    [25,10],
-    [50,10],
-]
+const PTS = [
+    [-46, 23],
+];
+var A,B;
 
 function update(width=0.8){
     // y = a (x-c) + b
     var x0 = 0;
-    var y0 = Number(Math.tan(input_a.value))*(50-Number(input_c.value)) + Number(input_b.value) +50;
+    A = Number(Math.tan(input_a.value));
+    B = A*Number(input_c.value) + Number(input_b.value)
+    var y0 = A*(50+Number(input_c.value)) + Number(input_b.value) +50;
     var x1 = 100;
-    var y1 = Number(Math.tan(input_a.value))*(-50-Number(input_c.value)) + Number(input_b.value) +50;
+    var y1 = A*(-50+Number(input_c.value)) + Number(input_b.value) +50;
     var dx = x1-x0;
     var dy = y1-y0;
     line.outerHTML = `<path style="stroke:#000000;stroke-width:${width};stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
