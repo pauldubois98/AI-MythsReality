87313955caa9b24eb26a62f65994c4d89bd764b3
pauldubois98 <pauldubois98@gmail.com>
2023-05-18 13:35:27 +0200

    input adjusts v2 (tan)

--- BlackBox/script.js
+++ BlackBox/script.js
@@ -2,9 +2,9 @@
 function update(){
     // y = a (x-c) + b
     var x0 = 0;
-    var y0 = Number(input_a.value)*Number(input_a.value)*Number(input_a.value)*(50-Number(input_c.value)) + Number(input_b.value) +50;
+    var y0 = Number(Math.tan(input_a.value))*(50-Number(input_c.value)) + Number(input_b.value) +50;
     var x1 = 100;
-    var y1 = Number(input_a.value)*Number(input_a.value)*Number(input_a.value)*(-50-Number(input_c.value)) + Number(input_b.value) +50;
+    var y1 = Number(Math.tan(input_a.value))*(-50-Number(input_c.value)) + Number(input_b.value) +50;
     var dx = x1-x0;
     var dy = y1-y0;
     line.outerHTML = `<path style="stroke:#000000;stroke-width:0.3;stroke-linecap:round" d="m ${x0},${y0} ${dx},${dy}" id="line" sodipodi:nodetypes="cc"></path>`;
